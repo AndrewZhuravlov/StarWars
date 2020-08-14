bddc40911bb47dc6bc9e1d99f669806d1fc468c3
AndrewZhuravlov <63732647+AndrewZhuravlov@users.noreply.github.com>
2020-08-14 13:30:23 +0300

    Start

---
+++ star-wars/src/components/app/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import s from './App.css'
+
+export default class App extends Component {
+
+    render() {
+
+        return (
+        <div>
+            <h1>hello pidr</h1>
+            <ul>
+                <li>syka</li>
+                <li>bl9</li>
+                <li>noyt</li>
+                <li>ne </li>
+                <li>nyzno</li>
+            </ul>
+        </div>
+        )
+    }
+}
